b3958a170dd847ea4841ccf1ba6d42ac98dc7e6e
boredradrad <radwa.20399305@compit.aun.edu.eg>
2021-11-01 22:49:09 +0200

    second

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 5, y = 3;
+
+	cout << x + y;
+}
+
